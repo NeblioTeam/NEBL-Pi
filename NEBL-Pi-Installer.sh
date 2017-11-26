a04b728f08c04ddeba7da775e2bca8f3bcee68de
NeblioTeam <neblioteam@gmail.com>
2017-11-26 15:05:19 -0500

    Add libssl-dev install for jessie

--- NEBL-Pi-Installer.sh
+++ NEBL-Pi-Installer.sh
@@ -55,6 +55,7 @@ done
 sudo apt-get update -y
 sudo apt-get install build-essential -y
 sudo apt-get install libboost-all-dev -y
+sudo apt-get install libssl-dev -y
 sudo apt-get install libdb++-dev -y
 sudo apt-get install libminiupnpc-dev -y
 sudo apt-get install libqrencode-dev -y
