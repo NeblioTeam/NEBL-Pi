f184dc8b02e967e73339565761c6edbb2c23137d
Neblio Team <neblioteam@gmail.com>
2018-01-19 21:49:52 -0500

    Update to v0.4

--- NEBL-Pi-Installer.sh
+++ NEBL-Pi-Installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#NEBL-Pi Installer v0.3
+#NEBL-Pi Installer v0.4
 
 echo "================================================================================"
 echo "=================== Welcome to the Ofiicial NEBL-Pi Installer =================="
